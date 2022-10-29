c23e3fedacba6004d9512c12a5639470aeac0d22
Md Solyman Hossen <105260985+solymanHossen@users.noreply.github.com>
2022-10-30 00:08:01 +0600

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="col-md-3">
+    <section class="button">
+      <button @click="prev">Prev</button>
+      <h3>{{ currentMonthInName }} {{ currentYear }}</h3>
+      <button @click="next">Next</button>
+    </section>
+
+    <section>
+      <div class="days">
+        <p v-for="(day, index) in days" :key="index">{{ day }}</p>
+      </div>
+    </section>
+    <section>
+      <div class="date">
+        <p v-for="day in startDay()" :key="day"></p>
+        <p v-for="date in daysInMonth(currentYear, currentMonthInNumber)" :key="date" ref="date" @click="getDate">
+          {{ date }}
+        </p>
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentMonthInNumber: new Date().getMonth(),
+      currentYear: new Date().getFullYear(),
+      days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    };
+  },
+  methods: {
+    daysInMonth(year, month) {
+      return new Date(year, month + 1, 0).getDate();
+    },
+    startDay() {
+      return new Date(this.currentYear, this.currentMonthInNumber, 1).getDay();
+    },
+    next() {
+      if (this.currentMonthInNumber === 11) {
+        this.currentYear++
+        this.currentMonthInNumber = 0
+      } else {
+        this.currentMonthInNumber++;
+      }
+    }, prev() {
+      if (this.currentMonthInNumber === 0) {
+        this.currentYear--;
+        this.currentMonthInNumber = 11
+      }
+      else {
+        this.currentMonthInNumber--;
+      }
+      console.log(this.currentMonthInNumber)
+    },
+    getDate() {
+      console.log(this.$refs.date)
+      return
+    }
+  },
+  computed: {
+    currentMonthInName() {
+      return new Date(this.currentYear, this.currentMonthInNumber).toLocaleString("default", { month: "long" })
+    }
+  }
+};
+</script>
+
+<style>
+.days,
+.date {
+  text-align: center;
+  display: flex;
+}
+
+.date {
+  flex-wrap: wrap;
+}
+
+.days p,
+.date p {
+  width: 14.28%;
+
+}
+
+.days p,
+.date p:hover {
+  /* color: red; */
+
+}
+
+h1 {
+  text-align: center;
+}
+
+.button {
+  display: flex;
+  justify-content: space-between;
+}
+
+button {
+  cursor: pointer;
+}
+
+/* .main-Section {
+  width: 400px;
+} */
+</style>
